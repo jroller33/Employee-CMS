13e76bc8ae55a5c7cb420bed2c739ea93fd7c171
John Roller <jtroller33@gmail.com>
2022-08-18 18:04:19 -0500

    Department module

--- index.js
+++ index.js
@@ -17,21 +17,25 @@ function mainMenu() {
         ],
     }).then(response => {
         if (response.menu === "View All Employees") {
+            // print out table w all employees 21 sec
+            mainMenu();
 
         } else if (response.menu === "Add Employee") {
 
         } else if (response.menu === "Update Employee Role") {
 
         } else if (response.menu === "View All Roles") {
+            // print out table with all jobs, what dept it is and salary (14 sec)
+            mainMenu();
 
         } else if (response.menu === "Add Role") {
 
         } else if (response.menu === "View All Departments") {
             // print out table showing all departments (7 sec in video)
             mainMenu();
-            
-        } else if (response.menu === "Add department") {
 
+        } else if (response.menu === "Add department") {
+            
         } else if (response.menu === "Quit") {
 
         } else {

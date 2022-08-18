71c106a8064a6d3e59a514d8bbef516a6a0c78aa
John Roller <jtroller33@gmail.com>
2022-08-18 17:27:02 -0500

    writing inquirer prompts

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require("inquirer");
+
+function mainMenu() {
+    inquirer.createPromptModule({
+        type: 'list',
+        name: 'menu',
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add department",
+            "Quit"            
+        ],
+    }).then(response => {
+        if (response.menu === "View All Employees") {
+
+        } else if (response.menu === "Add Employee") {
+
+        } else if (response.menu === "Update Employee Role") {
+
+        } else if (response.menu === "View All Roles") {
+
+        } else if (response.menu === "Add Role") {
+
+        } else if (response.menu === "View All Departments") {
+            // print out table showing all departments (7 sec in video)
+            mainMenu();
+            
+        } else if (response.menu === "Add department") {
+
+        } else if (response.menu === "Quit") {
+
+        } else {
+            console.log("error in mainMenu() if/else statements");
+        }
+    })
+}

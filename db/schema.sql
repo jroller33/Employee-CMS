561349d26fda060eac3e674bdf154aec71dcc192
John Roller <jtroller33@gmail.com>
2022-08-19 12:10:58 -0500

    created tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,23 @@
-DROP DATABASE IF EXISTS cms;
-CREATE DATABASE cms;
+-- to run: >>> SOURCE ./db/schema.sql
+
+DROP DATABASE IF EXISTS cms_db;
+CREATE DATABASE cms_db;
+USE cms_db;
+CREATE TABLE department {
+    id INT,         -- department_id in role table
+    name VARCHAR(30),   
+};
+CREATE TABLE role{
+    id INT,         -- role_id in employee table
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+};
+
+CREATE TABLE employee{
+    id INT, -- ??? in employee table manager_id is linked to id
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,  -- ??? in employee table manager_id is linked to id
+};

9875966452e38066b6be7e2d9c22e0fc0b6031e0
John Roller <jtroller33@gmail.com>
2022-08-19 12:20:35 -0500

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,21 @@
 DROP DATABASE IF EXISTS cms_db;
 CREATE DATABASE cms_db;
 USE cms_db;
-CREATE TABLE department {
+CREATE TABLE department (
     id INT,         -- department_id in role table
-    name VARCHAR(30),   
-};
-CREATE TABLE role{
+    name VARCHAR(30)   
+);
+CREATE TABLE role (
     id INT,         -- role_id in employee table
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
-};
+    department_id INT
+);
 
-CREATE TABLE employee{
+CREATE TABLE employee (
     id INT, -- ??? in employee table manager_id is linked to id
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,  -- ??? in employee table manager_id is linked to id
-};
+    manager_id INT  -- ??? in employee table manager_id is linked to id
+);

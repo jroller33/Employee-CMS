31ce2310e104b7ed91a56b02df567aff99531d8d
John Roller <jtroller33@gmail.com>
2022-08-19 12:49:27 -0500

    db and sql started

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,12 @@
 DROP DATABASE IF EXISTS cms_db;
 CREATE DATABASE cms_db;
 USE cms_db;
+
 CREATE TABLE department (
     id INT,         -- department_id in role table
     name VARCHAR(30)   
 );
+
 CREATE TABLE role (
     id INT,         -- role_id in employee table
     title VARCHAR(30),

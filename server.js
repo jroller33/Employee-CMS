a93e1a57b96f5a11da303142572d94d7345b9bde
John Roller <jtroller33@gmail.com>
2022-08-20 23:23:02 -0500

    .

--- server.js
+++ server.js
@@ -1,31 +1,52 @@
-const express = require('express');
-const mysql = require('mysql2');
-// require sequelize
+// const express = require('express');
+// const mysql = require('mysql2');
+// // require sequelize
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-const db = mysql.createConnection(
-  {
-    host: 'localhost',      
-    user: 'root',
-    password: 'mysqlPass',  // put in .env
-    database: 'cms_db'
-  },
-  console.log(`Connected to the cms_db database.`)
-);
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',      
+//     user: 'root',
+//     password: 'mysqlPass',  // put in .env
+//     database: 'cms_db'
+//   },
+//   console.log(`Connected to the cms_db database.`)
+// );
 
-db.query('SELECT * FROM students', function (err, results) {
-  console.log(results);
-});
+// // Query database
+// // let deletedRow;
+// // db.query(`DELETE FROM favorite_books WHERE id = ?`, deletedRow, (err, result) => {
+// //   if (err) {
+// //     console.log(err);
+// //   }
+// //   console.log(result);
+// // });
 
-// app.use((req, res) => {
-//   res.status(404).end();
+// // db.query('SELECT * FROM favorite_books', function (err, results) {
+// //   console.log(results);
+// // });
+
+// // // Default response for any other request (Not Found)
+// // app.use((req, res) => {
+// //   res.status(404).end();
+// // });
+
+// // db.query('SELECT * FROM students', function (err, results) {
+// //   console.log(results);
+// // });
+
+
+
+
+// // app.use((req, res) => {
+// //   res.status(404).end();
+// // });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
 // });
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
